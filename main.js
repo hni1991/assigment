79ab43d8a1229f2f218b50cca778aa0ed21369a5
Hossein <hoseinnikoo@hotmail.com>
2019-09-12 11:29:07 +0200

    second update

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 //object from server
-
 $SPECDT = [{
         group: 'General',
         items: [{
@@ -28,33 +27,38 @@ $SPECDT = [{
 
 // Changes when we reach the min-width:768
 $(function ($) {
-    $triger = false
-
-    function mediaSize() {
-        if (window.matchMedia('(min-width: 768px)').matches) {
-            $triger = false
-            if ($triger) {
-                null
-            } else {
-                CRN()
-                $triger = true
-            }
-        } else {
-            $triger = true
-            if (!$triger) {
-                null
-            } else {
-                CRR()
-                $triger = false
-            }
-        }
-    };
-    mediaSize();
-    window.addEventListener('resize', mediaSize);
+    elementCreate();
+    window.addEventListener('resize',elementCreate );
 })
 
-
-
+//in this case i'm useing SPECDT object (Array type) as object from server
+function elementCreate() {
+    $triger = false
+    //normali hier this function call ajax function and as promis or async waite for object and then start to create
+    $param = $SPECDT
+if($param){
+    if (window.matchMedia('(min-width: 768px)').matches) {
+        $triger = false
+        if ($triger) {
+            null
+        } else {
+            normalCreate($param)
+            $triger = true
+        }
+    } else {
+        $triger = true
+        if (!$triger) {
+            null
+        } else {
+            responsCreate($param)
+            $triger = false
+        }
+    }
+}else{
+    //hier can be create error 
+    null
+}
+};
 // toggle function
 function toggle(e, togglename) {
     for (let i = 0; i < $('.Tab-content').length; i++) {
@@ -68,37 +72,35 @@ function toggle(e, togglename) {
 }
 
 // elemnt creator for normal size
-//in this case i'm useing SPECDT object (Array type) as object from server
-function CRN() {
-
+function normalCreate(obj) {
     $('#spec').html('')
     $('#spec').append('<h2>AUFGABE</h2>')
     $('#spec').append('<p>Ein Traum auf vier R&auml;dern...</p>')
     $('#spec').append('<div class="Btn-cont" id="Btncont"></div>')
 
 
-    for (let i = 0; i < $SPECDT.length; i++) {
+    for (let i = 0; i < obj.length; i++) {
         if (i === 0) {
-            $('#Btncont').append("<button class='tablinks active' style='display: block' onclick='toggle(event," + $SPECDT[i].group + ")'>" + $SPECDT[i].group + "</button>")
-            if ($SPECDT[i].items) {
+            $('#Btncont').append("<button class='tablinks active' style='display: block' onclick='toggle(event," + obj[i].group + ")'>" + obj[i].group + "</button>")
+            if (obj[i].items) {
                 $var1 = ''
-                for (let x = 0; x < $SPECDT[i].items.length; x++) {
-                    $var1 += '<dt>' + $SPECDT[i].items[x].name + '</dt><dd>' + $SPECDT[i].items[x].value + '</dd>'
+                for (let x = 0; x < obj[i].items.length; x++) {
+                    $var1 += '<dt>' + obj[i].items[x].name + '</dt><dd>' + obj[i].items[x].value + '</dd>'
                 }
-                $('#spec').append('<div id="' + $SPECDT[i].group + '" style="display: block" class="Tab-content "><dl>' + $var1 + '</dl></div>')
+                $('#spec').append('<div id="' + obj[i].group + '" style="display: block" class="Tab-content "><dl>' + $var1 + '</dl></div>')
             } else {
-                $('#spec').append('<div id="' + $SPECDT[i].group + '" style="display: block" class="Tab-content "><span>' + $SPECDT[i].text + '</span></div>')
+                $('#spec').append('<div id="' + obj[i].group + '" style="display: block" class="Tab-content "><span>' + obj[i].text + '</span></div>')
             }
         } else {
-            $('#Btncont').append('<button class="tablinks"  onclick="toggle(event, ' + $SPECDT[i].group + ')">' + $SPECDT[i].group + '</button>')
-            if ($SPECDT[i].items) {
+            $('#Btncont').append('<button class="tablinks"  onclick="toggle(event, ' + obj[i].group + ')">' + obj[i].group + '</button>')
+            if (obj[i].items) {
                 $var1 = ''
-                for (let x = 0; x < $SPECDT[i].items.length; x++) {
-                    $var1 += '<dt>' + $SPECDT[i].items[x].name + '</dt><dd>' + $SPECDT[i].items[x].value + '</dd>'
+                for (let x = 0; x < obj[i].items.length; x++) {
+                    $var1 += '<dt>' + obj[i].items[x].name + '</dt><dd>' + obj[i].items[x].value + '</dd>'
                 }
-                $('#spec').append('<div id="' + $SPECDT[i].group + '"  class="Tab-content "><dl>' + $var1 + '</dl></div>')
+                $('#spec').append('<div id="' + obj[i].group + '"  class="Tab-content "><dl>' + $var1 + '</dl></div>')
             } else {
-                $('#spec').append('<div id="' + $SPECDT[i].group + '"  class="Tab-content "><span>' + $SPECDT[i].text + '</span></div>')
+                $('#spec').append('<div id="' + obj[i].group + '"  class="Tab-content "><span>' + obj[i].text + '</span></div>')
 
             }
         }
@@ -108,34 +110,34 @@ function CRN() {
 
 // elemnt creator for small or responsive size
 
-function CRR() {
+function responsCreate(obj) {
     $('#spec').html('')
     $('#spec').append('<h2>AUFGABE</h2>')
     $('#spec').append('<p>Ein Traum auf vier R&auml;dern...</p>')
     $('#spec').append('<div class="Btn-cont" id="Btncont"></div>')
 
-    for (let i = 0; i < $SPECDT.length; i++) {
+    for (let i = 0; i < obj.length; i++) {
         if (i === 0) {
-            $('#Btncont').append("<button class='tablinks active' style='display: block' onclick='toggle(event," + $SPECDT[i].group + ")'>" + $SPECDT[i].group + "</button>")
-            if ($SPECDT[i].items) {
+            $('#Btncont').append("<button class='tablinks active' style='display: block' onclick='toggle(event," + obj[i].group + ")'>" + obj[i].group + "</button>")
+            if (obj[i].items) {
                 $var1 = ''
-                for (let x = 0; x < $SPECDT[i].items.length; x++) {
-                    $var1 += '<dt>' + $SPECDT[i].items[x].name + '</dt><dd>' + $SPECDT[i].items[x].value + '</dd>'
+                for (let x = 0; x < obj[i].items.length; x++) {
+                    $var1 += '<dt>' + obj[i].items[x].name + '</dt><dd>' + obj[i].items[x].value + '</dd>'
                 }
-                $('#Btncont').append('<div id="' + $SPECDT[i].group + '" style="display: block" class="Tab-content "><dl>' + $var1 + '</dl></div>')
+                $('#Btncont').append('<div id="' + obj[i].group + '" style="display: block" class="Tab-content "><dl>' + $var1 + '</dl></div>')
             } else {
-                $('#Btncont').append('<div id="' + $SPECDT[i].group + '" style="display: block" class="Tab-content "><span>' + $SPECDT[i].text + '</span></div>')
+                $('#Btncont').append('<div id="' + obj[i].group + '" style="display: block" class="Tab-content "><span>' + obj[i].text + '</span></div>')
             }
         } else {
-            $('#Btncont').append('<button class="tablinks"  onclick="toggle(event, ' + $SPECDT[i].group + ')">' + $SPECDT[i].group + '</button>')
-            if ($SPECDT[i].items) {
+            $('#Btncont').append('<button class="tablinks"  onclick="toggle(event, ' + obj[i].group + ')">' + obj[i].group + '</button>')
+            if (obj[i].items) {
                 $var1 = ''
-                for (let x = 0; x < $SPECDT[i].items.length; x++) {
-                    $var1 += '<dt>' + $SPECDT[i].items[x].name + '</dt><dd>' + $SPECDT[i].items[x].value + '</dd>'
+                for (let x = 0; x < obj[i].items.length; x++) {
+                    $var1 += '<dt>' + obj[i].items[x].name + '</dt><dd>' + obj[i].items[x].value + '</dd>'
                 }
-                $('#Btncont').append('<div id="' + $SPECDT[i].group + '"  class="Tab-content "><dl>' + $var1 + '</dl></div>')
+                $('#Btncont').append('<div id="' + obj[i].group + '"  class="Tab-content "><dl>' + $var1 + '</dl></div>')
             } else {
-                $('#Btncont').append('<div id="' + $SPECDT[i].group + '"  class="Tab-content "><span>' + $SPECDT[i].text + '</span></div>')
+                $('#Btncont').append('<div id="' + obj[i].group + '"  class="Tab-content "><span>' + obj[i].text + '</span></div>')
 
             }
         }
